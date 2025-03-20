47695f057a1cbe61365735519bc330a70d174cfd
VictorMirandaO <victoroliveiragit@gmail.com>
2025-03-20 18:33:58 -0300

    novo

---
+++ joyclass atividades/atividade_array.py
@@ -0,0 +1,9 @@
+lista_musicos = ["Djavan", "Roberto Carlos", "Elis Regina", "Tom Jobim", 
+"Milton Nascimento", "Chico Buarque", "Nara Leão", "Pitty", "Simonal",
+"Moacir Santos", "Caetano Veloso", "Elza Soares", "Paulinho da Viola",
+"Yamandú Costa", "Gal Costa"]
+
+print("A quantidade de elementos são:", len(lista_musicos))
+print("O segundo músico é", lista_musicos[1])
+print("O nono músico é", lista_musicos[8])
+print("O décimo quarto músico é", lista_musicos[13])
