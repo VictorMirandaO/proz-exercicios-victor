8ce2ccce843c08f255e79d9ca4a00891833adf6c
VictorMirandaO <victoroliveiragit@gmail.com>
2025-03-20 18:48:16 -0300

    novo

---
+++ joyclass atividades/atividade2_array.py
@@ -0,0 +1,7 @@
+lista_produtos = ["máscaras faciais", "batons", "esmaltes", "perfumes", "loções", "xampus", "sabonetes", "deliniadores"]
+
+for produto in lista_produtos:
+    print(produto)
+    
+for produto in lista_produtos:
+    print(f"Temos {produto} à venda!")
