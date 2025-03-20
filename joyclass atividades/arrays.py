4a316043546b1caa6850e2af17ff6e9e62e3c02b
VictorMirandaO <victoroliveiragit@gmail.com>
2025-03-20 18:15:26 -0300

    novo

---
+++ joyclass atividades/arrays.py
@@ -0,0 +1,5 @@
+strings_alimentos = ["hamburguer", "pizza", "taco", "lasanha", "batata"]
+num_int_nascimento = ["1982", "2007", "2008", "2013", "2021"]
+
+print(f"Os alimentos: {strings_alimentos}")
+print(f"Os anos de nascimento: {num_int_nascimento}")
